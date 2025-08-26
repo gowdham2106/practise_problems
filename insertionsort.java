987c508866876a3f038620a38add4a12ad389135
G Gowdham <gowdhamgokuldas@gmail.com>
2025-08-26 20:40:11 +0530

    insertion sort

--- insertionsort.java
+++ insertionsort.java
@@ -1,16 +1,16 @@
-package daily_task;
+package dailyprblm;
 
 import java.util.Arrays;
 
-class Selectionsort {
+public class insertionsort {
 public static void main(String[] args) {
-	int[] arr= {5,2,9,1,5,6};
+	int[] arr= {5,6,4,2,3,1};
 	for (int i = 1; i < arr.length; i++) {
 		int j=i-1;
 		int key=arr[i];
-		while(j>=0 && arr[j]>key) {
-		arr[j+1]=arr[j];
-		j--;
+		while(j>=0&&arr[j]>key) {
+			arr[j+1]=arr[j];
+			j--;
 		}
 		arr[j+1]=key;
 	}
